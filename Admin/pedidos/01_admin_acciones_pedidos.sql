7831170582ece660a7afd96ac6f05b9084fa76cb
AleVC <alejandroavasquezc@gmail.com>
2025-11-09 20:37:20 -0300

    cambiado/ ? --> id de ejemplo

--- Admin/pedidos/01_admin_acciones_pedidos.sql
+++ Admin/pedidos/01_admin_acciones_pedidos.sql
@@ -16,27 +16,27 @@ SELECT DISTINCT estado FROM pedidos;
 -- A.1) Cambiar a 'espera'
 -- Reemplaza ? por el ID real del pedido
 -- antes de actualizar, puedes verificar que el pedido existe
-SELECT id, estado FROM pedidos WHERE id = ?;
+SELECT id, estado FROM pedidos WHERE id = 2;
 UPDATE pedidos
 SET estado = 'espera'
-WHERE id = ?;
+WHERE id = 2;
 
 -- A.2) Cambiar a 'produccion'
-SELECT id, estado FROM pedidos WHERE id = ?;
+SELECT id, estado FROM pedidos WHERE id = 1;
 UPDATE pedidos
 SET estado = 'produccion'
-WHERE id = ?;
+WHERE id = 1;
 
 -- A.3) Cambiar a 'retirar'   (armonizado con el front)
-SELECT id, estado FROM pedidos WHERE id = ?;
+SELECT id, estado FROM pedidos WHERE id = 1;
 UPDATE pedidos
 SET estado = 'retirar'
-WHERE id = ?;
+WHERE id = 1;
 
 -- A.4) Cambiar a 'cancelado'   (armonizado con el front)
-SELECT id, estado FROM pedidos WHERE id = ?;
+SELECT id, estado FROM pedidos WHERE id = 1;
 UPDATE pedidos
 SET estado = 'cancelado'
-WHERE id = 2;
+WHERE id = 1;
 
 select * from pedidos;
