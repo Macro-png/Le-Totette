dd2259737aaac4f6d96a94b170042310d0fab6bb
Macro <macrogames007@gmail.com>
2025-11-09 21:27:53 -0300

    Fixed add_color

--- Admin/crear producto/add_color.sql
+++ Admin/crear producto/add_color.sql
@@ -7,11 +7,13 @@ describe colores;
 select * from colores;
 
 insert into colores
-(productos_id, codigo_hexa)
+(id, productos_id, codigo_hexa)
 values
-(1, "#000000"),
-(1, "#FFFFFF"),
-(2, "#FF69B4"),
-(3, "#008080");
+(NULL, 1, "#000000"),
+(NULL, 1, "#FFFFFF"),
+(NULL, 2, "#FF69B4"),
+(NULL, 3, "#008080");
 
 select * from colores;
+
+delete from colores;
