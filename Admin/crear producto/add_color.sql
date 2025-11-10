9193de0ef7932bbcc2504a65069ebbbb29d0994a
Macro <macrogames007@gmail.com>
2025-11-10 00:32:54 -0300

    Fixed add_prod, mod_prod

--- Admin/crear producto/add_color.sql
+++ Admin/crear producto/add_color.sql
@@ -12,7 +12,8 @@ values
 (NULL, 1, "#000000"),
 (NULL, 1, "#FFFFFF"),
 (NULL, 2, "#FF69B4"),
-(NULL, 3, "#008080");
+(NULL, 3, "#008080"),
+(NULL, 4, "#000000");
 
 select * from colores;
 
--- Admin/mod producto/mod_product.sql
+++ Admin/mod producto/mod_product.sql
@@ -16,4 +16,22 @@ set
     descripcion = "Versión mejorada del bolso tote floral, con nuevos colores y materiales de alta calidad."
 where id = 2;
 
+insert into filtros
+(productos_id, filtro)
+values
+(2, "deluxe");
+
+select
+    p.id as producto_id,
+    p.nombre as nombre_producto,
+    p.precio_unidad,
+    group_concat(distinct f.filtro order by f.filtro separator ', ') as filtros,
+    group_concat(distinct c.codigo_hexa order by c.codigo_hexa separator ', ') as colores
+from productos p
+left join filtros f on p.id = f.productos_id
+left join colores c on p.id = c.productos_id
+-- where filtro = 'abstracto' -- SOLO MOSTRAR PRODUCTOS CON FILTRO ABSTRACTO
+group by p.id, p.nombre, p.precio_unidad
+order by p.id;
+
 select * from productos where id = 2;
